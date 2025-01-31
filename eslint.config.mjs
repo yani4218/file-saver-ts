c985c309ce50b4d4bf416aa9e854f5cfa0e42d1f
Yanis Seylis <yseylis@ptsecurity.com>
2025-01-31 20:14:42 +0300

    update deps.

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+    rules: { 'no-undef': 'warn' }
+});
--- src/lib/utils/ms.ts
+++ src/lib/utils/ms.ts
@@ -1,5 +1,6 @@
 // Download files for IE or Edge(not chromium) browser
 export const msBlobDownload = (blob: Blob, filename: string): void => {
-  const navigator: any = window.navigator;
-  navigator.msSaveBlob(blob, filename);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const navigator: any = window.navigator;
+    navigator.msSaveBlob(blob, filename);
 };
